8b814db8ceb7440883d1cfc9c679f18cffd89907
JasonAHardja <jason.hardjawidjaja@gmail.com>
2025-06-04 17:50:36 +0700

    new

--- .Backend/server.js
+++ .Backend/server.js
@@ -26,7 +26,7 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use('/api/tickets', ticketRoutes);
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
